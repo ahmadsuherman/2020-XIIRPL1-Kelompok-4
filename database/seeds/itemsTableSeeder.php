9ca4c1ea3d32b9a0d8a09e6dd70083d2d248ddd6
SantiSintia <santayy2003@gmail.com>
2020-08-03 17:08:05 +0700

    Make a seeder

---
+++ database/seeds/itemsTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class itemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->insert([
+            'item_name'  => 'Kamera kecil',
+            'total_item' => 4,
+            'stock_item' => 4
+        ]);
+        DB::table('items')->insert([
+            'item_name'  => 'Bola basket',
+            'total_item' => 2,
+            'stock_item' => 2
+        ]);
+        DB::table('items')->insert([
+            'item_name'  => 'lighting',
+            'total_item' => 1,
+            'stock_item' => 1
+        ]);
+        DB::table('items')->insert([
+            'item_name'  => 'HDMI',
+            'total_item' => 2,
+            'stock_item' => 2
+        ]);
+        DB::table('items')->insert([
+            'item_name'  => 'Projector',
+            'total_item' => 4,
+            'stock_item' => 4
+        ]);
+    }
+}

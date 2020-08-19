059968ed2c0eba2b4453a9b4fb5f5aa637a80793
SantiSintia <santayy2003@gmail.com>
2020-08-19 10:23:58 +0700

    [F-2]Revisi Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Registration Page</title>
+  <title>Inventory | Registrasi</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -35,42 +35,61 @@
   </div>
 
   <div class="register-box-body">
-    <p class="login-box-msg">Silahkan Register Terlebih Dahulu</p>
+    <p class="login-box-msg">Register pengguna baru</p>
 
-    <form action="{{ route('register') }}" method="POST">
-    @csrf
-      <div class="form-group has-feedback">
-        <input type="text" name="name" class="form-control" placeholder="Full name" required autocomplete="name" autofocus>
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="email" name="email" class="form-control" placeholder="Email" required autocomplete="email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password" required autocomplete="password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" name="password-confirm" class="form-control" placeholder="Retype password" required autocomplete="new-password">
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-        <div class="checkbox icheck">
-            <label><i>Sudah punya akun?</i>
-              <a href="{{ route('login') }}"> Login
-            </label>
-          </div>
-         
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-12">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                              <div class="form-group has-feedback">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Nama Lengkap" value="" required autocomplete="name" autofocus>
+                                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                              </div>
+
+                              <div class="form-group has-feedback">                              
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"  placeholder="Alamat Email" value="" required autocomplete="email">
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                              </div>
+
+                              <div class="form-group has-feedback">                             
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="new-password">
+                                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                              </div>
+
+                              <div class="form-group has-feedback">                             
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Konfirmasi Password" required autocomplete="new-password">
+                                <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                              </div>  
+
+                        <div class="form-group row mb-0">
+                            <div class="col-xs-4">
+                                <button type="submit" class="btn btn-primary btn-block btn-flat">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
   </div>
   <!-- /.form-box -->
 </div>
